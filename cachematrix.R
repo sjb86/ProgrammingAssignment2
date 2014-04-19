922f5ae90d68896a86066169bd2a596ee3d2c017
Sebastian Bray <sebjbray@gmail.com>
2014-04-19 11:57:56 +0100

    update progR

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## makeCacheMatrix when passed a matrix will create an object of class list
+## of which the elements are 4 functions tied to the matrix passed.
+## These functions allow the setting/getting of inverse of the matrix passed
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setsolve <- function(solve) m <<- solve
+        getsolve <- function() m
+        list(set = set, get = get,
+             setsolve = setsolve,
+             getsolve = getsolve)
 }
 
-
-## Write a short comment describing this function
+#cacheSolve when passed an object outputted by makeCacheMatrix (which in turn required an invertible matrix)
+#will check to see if it already has a cached value of the inverse, and if not it will calculate the inverse
+#and cache the result.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getsolve()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setsolve(m)
+        m
 }
